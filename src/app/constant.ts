6ff9ee2236a8b4cef3f02c6d88eb8ca410d6688f
Antonio Tomaquin <antoniotomaquin@gmail.com>
2025-06-04 15:00:39 +0800

    add constant config
    add

--- src/app/constant.ts
+++ src/app/constant.ts
@@ -1,5 +1,8 @@
 //LOCALHOST
 export const SERVER_URL = 'http://localhost:0102';
+
 //UAT
 //TOMCAT     
   // export const SERVER_URL = 'http://10.2.1.59:8090/helpdesk-server';
+
+// export const SERVER_URL = 'https://springapi-b2z9.onrender.com';
